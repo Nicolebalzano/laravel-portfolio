835e51842aba92598d7ccfe92c5d6216bde7a85c
Nicolebalzano <nicole.balzano03@gmail.com>
2025-09-01 10:29:08 +0200

    feat:verificazione aggiunta

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::resource("projects", ProjectController::class);
+Route::resource("projects", ProjectController::class)
+->middleware(['auth', 'verified']);
 require __DIR__.'/auth.php';
